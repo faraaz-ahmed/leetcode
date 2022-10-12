76be9bd00357b296e94a193c70eeab0c0ffbb8cd
faraaz-ahmed <faraazahmed000@gmail.com>
2022-10-12 09:51:29 +0100

    12Oct2022

--- DynamicProgramming/elements_of_programming_interviews/NumberOfScoreCombinations.py
+++ DynamicProgramming/elements_of_programming_interviews/NumberOfScoreCombinations.py
@@ -3,5 +3,8 @@ def solution(final_score, scores):
   for i in range(0, len(scores)):
     for j in range(1, final_score + 1):
       without_this_play = (num_combinations_for_score[i - 1][j] if i >= 1 else 0)
-      with_this_play = num_combinations_for_score[i][j - scores[i]] if j >= scores[i] else 0 + num_combinations_for_score[i - 1][j] if i >= 0 else 0
+      with_this_play = num_combinations_for_score[i][j - scores[i]] if j >= scores[i] else 0
       num_combinations_for_score[i][j] = without_this_play + with_this_play
+      print(num_combinations_for_score)
+  return num_combinations_for_score[-1][-1]
+solution(12, [2,3,7])
